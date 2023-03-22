19a04255f8909321f75d7467797b3c4bdb4c9cdd
hgutierrez1 <hgutierrezva123@gmail.com>
2023-03-21 21:03:34 -0500

    add product list aside

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const email=document.querySelector('.navabar-email')
 const arrowdown=document.querySelector(".arrowdown");
 const desktopMenu=document.querySelector(".desktop-menu");
+const carritocompra=document.querySelector('.navbar-shopping-cart');
+const listacarrito=document.querySelector('.product-detail');
 
 arrowdown.addEventListener("click",togledesktopMenu);
-email.addEventListener('click',togledesktopMenu)
+email.addEventListener('click',togledesktopMenu);
+carritocompra.addEventListener('click',togglelistacarrito)
 
 function togledesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+    listacarrito.classList.add('inactive');
+}
+function togglelistacarrito(){
+    listacarrito.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
 }
