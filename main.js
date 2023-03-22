7dad853139cd6f04443b77ab2281ffc502de9850
hgutierrez1 <hgutierrezva123@gmail.com>
2023-03-22 18:38:37 -0500

    completado funcionalidad de descripcion del producto

--- main.js
+++ main.js
@@ -4,12 +4,17 @@ const desktopMenu=document.querySelector(".desktop-menu");
 const carritocompra=document.querySelector('.navbar-shopping-cart');
 const listacarrito=document.querySelector('.product-detail');
 const mobilemenuicon = document.querySelector('.menu-icon');
-const mobilemenu = document.querySelector('.mobile-menu')
+const mobilemenu = document.querySelector('.mobile-menu');
+const productData=document.querySelector('.product-data');
+const closeButton=document.querySelector('.close-button')
+const product=document.querySelectorAll('.product-card');
 
 arrowdown.addEventListener("click",togledesktopMenu);
 email.addEventListener('click',togledesktopMenu);
 carritocompra.addEventListener('click',togglelistacarrito)
 mobilemenuicon.addEventListener('click',togglemenumobile);
+closeButton.addEventListener('click',closeproductData);
+product.forEach(prod=>prod.addEventListener('click',openproductData));
 
 function togledesktopMenu(){
     desktopMenu.classList.toggle('inactive');
@@ -23,4 +28,12 @@ function togglelistacarrito(){
 function togglemenumobile(){
     mobilemenu.classList.toggle('inactive');
     listacarrito.classList.add('inactive');
+    productData.classList.add('inactive')
+}
+function closeproductData(){
+    productData.classList.add('inactive')
+}
+function openproductData(){
+    productData.classList.remove('inactive');
+    mobilemenu.classList.add('inactive')
 }
